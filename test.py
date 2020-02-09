8e42cc8067542476a64c806d2c3b4cf9e7c0d8dd
didgogns <34927608+didgogns@users.noreply.github.com>
2020-02-09 23:15:05 +0900

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,9 @@ from heapq import *
 from edge_scores import *
 from DSFNode import *
 import sys
+import copy
 import string
+import constants
 
 def dsf_reconstruct_dataset():
 	type2 = True
@@ -73,6 +75,7 @@ def dsf_reconstruct_dataset():
 		cv2.imwrite(reconDir + 'trim_index_grid.jpg', pixels)
 		filled_index_grid = forest.fill(trim_index_grid, extra_piece_list, img)
 		pixels = forest.reconstruct_trim(filled_index_grid, img.pieces)
+
 		cv2.imwrite(reconDir + 'filled_index_grid.jpg', pixels)
 
 		correct = direct_metric(img, filled_index_grid)
@@ -85,21 +88,22 @@ def dsf_reconstruct_dataset():
 	print 'direct metric', direct_metric_vals
 	print 'neighbor metric', neighbor_metric_vals
 
-def dsf_reconstruction_test():
+def dsf_reconstruction_test(img_filename):
 
 
 
-	type2 = True
+	type2 = False
 	random.seed(1122334455667)
 	picName = img_filename[string.rfind(img_filename, '/')+1:string.rfind(img_filename, '.')]
-	reconDir = './Images/Reconstruction_Generations/' + picName + '/'
+	reconDir = './Images/Reconstruction_Generations_' + str(P) + '_t/' + picName + '/'
+	print reconDir
 	if not os.path.exists(reconDir):
 		os.makedirs(reconDir)
 
 	img = Image(img_filename)
 	frame_W = img.W / P
 	frame_H = img.H / P
-	min_edges = min(min_edges, 2*frame_W*frame_H - frame_H - frame_W)
+	min_edges = min(constants.min_edges, 2*frame_W*frame_H - frame_H - frame_W)
 	scramble(img, type2)
 	assemble_image(img, reconDir + 'beginning.jpg')
 	K = len(img.pieces)
@@ -111,56 +115,116 @@ def dsf_reconstruction_test():
 	# dists_mgc = np.load('./Data/6.npy')
 
 	# dists_list = []
-	dists_list = compute_edge_dist(img, type2)
+	orig_dists_list = compute_edge_dist(img, type2)
 	# for i in xrange(K):
 	# 	for edgeNum_i in xrange(4):
 	# 		for j in xrange(K):
 	# 			for edgeNum_j in xrange(4):
 	# 				dists_list.append( (dists_mgc[i, edgeNum_i, j, edgeNum_j], i, j, edgeNum_i, edgeNum_j) )
-	heapify(dists_list)
+	best_extra = frame_W * frame_H
+	best_forest = None
+	best_grid = None
+	best_extra_list = None
+	threshold = frame_W * frame_H // 8 * 3
+	rep = 0
+	LR_W = 1
+	UD_W = 1
+	while rep < 11 or best_extra > threshold:
+		rep += 1
+		if rep == 32:
+			break
+		if LR_W + UD_W > 100:
+			return
+		dists_list = copy.deepcopy(orig_dists_list)
+		mag = random.random() * 0.1
+		if rep == 1:
+			mag = 0
+		print 'mag', mag, ', LR, UD', LR_W, UD_W
+		for dist in dists_list:
+			if dist[3] % 2  == 0:
+				dist[0] += random.random() * mag * LR_W + 0.01 * LR_W
+			else:
+				dist[0] += random.random() * mag * UD_W + 0.01 * UD_W
+		heapify(dists_list)
 
-	forest = DisjointSetForest(K)
+		forest = DisjointSetForest(K)
 
-	print 'starting with', len(dists_list), 'edges'
+		print 'starting with', len(dists_list), 'edges'
+		cnt = [0] * 2
 
-	while forest.numClusters > 1:
-		edge = heappop(dists_list)
-		clust_index = forest.union(edge[1], edge[2], edge[3], edge[4])
-		if clust_index != -1:
-			num_c = forest.numClusters
+		while forest.numClusters > 1 and len(dists_list) > 0:
+			edge = heappop(dists_list)
+			clust_index = forest.union(edge[1], edge[2], edge[3], edge[3])
+			if clust_index != -1:
+				num_c = forest.numClusters
+				cnt[edge[3] % 2] += 1
 
-			print 'reconstructing', num_c, edge
-			pixels = forest.reconstruct(clust_index, img.pieces)
-			# cv2.imwrite(reconDir + 'gen' + str(K - num_c) + '.cluster' + str(clust_index) + '.jpg', pixels)
+				if num_c < 50:
+					print 'reconstructing', num_c, edge
+				pixels = forest.reconstruct(clust_index, img.pieces)
+				# cv2.imwrite(reconDir + 'gen' + str(K - num_c) + '.cluster' + str(clust_index) + '.jpg', pixels)
 
-	print 'ended with', len(dists_list), 'edges'
+		print 'ended with', len(dists_list), 'edges'
+		if len(dists_list) == 0:
+			continue
 
-	forest.collapse()
+		forest.collapse()
 
-	for index in forest.pieceCoordMap.keys():
-		pixels = forest.reconstruct(index, img.pieces)
-		cv2.imwrite(reconDir + 'pre_trim.jpg', pixels)
+		index_grid, occupied_grid = forest.get_orig_trim_array()
+		index_grid = index_grid.astype(int)
+		print index_grid.shape
+		
+		pixels = forest.reconstruct_trim(index_grid, img.pieces)
 
-	index_grid, occupied_grid = forest.get_orig_trim_array()
-	pixels = forest.reconstruct_trim(index_grid, img.pieces)
 
-	
-	trim_index_grid, extra_piece_list = forest.trim(index_grid, occupied_grid, frame_W, frame_H)
+		trim_index_grid, extra_piece_list = forest.trim(index_grid, occupied_grid, frame_W, frame_H)
+		if trim_index_grid.shape[0] < frame_H or trim_index_grid.shape[1] < frame_H:
+			rep -= 1
+			print 'bad trim grid'
+			print 'cnt: ', cnt[0], cnt[1]
+			'''
+			if rep == 0:
+				if cnt[1] < cnt[0]:
+					LR_W += 1
+				elif cnt[0] < cnt[1]:
+					UD_W += 1
+			else:
+				if cnt[1] * 2 < cnt[0]:
+					LR_W += 1
+				elif cnt[0] * 2 < cnt[1]:
+					UD_W += 1
+			'''
+			if trim_index_grid.shape[0] < frame_H:
+				UD_W += 1
+			else:
+				LR_W += 1
+			continue
+		elif cnt[1] * 2 < cnt[0]:
+			LR_W += 1
+		elif cnt[0] * 2 < cnt[1]:
+			UD_W += 1
+		print 'iteration done', len(extra_piece_list), 'pieces out of place'
+		
+		if len(extra_piece_list) < best_extra:
+			best_extra, best_extra_list, best_forest, best_grid = len(extra_piece_list), extra_piece_list, forest, trim_index_grid
+			if rep > 15:
+				break
 
-	pixels = forest.reconstruct_trim(trim_index_grid, img.pieces)
+	if best_grid is None:
+		return
+	pixels = best_forest.reconstruct_trim(best_grid, img.pieces)
 	cv2.imwrite(reconDir + 'trim_index_grid.jpg', pixels)
-	filled_index_grid = forest.fill(trim_index_grid, extra_piece_list, img)
-	pixels = forest.reconstruct_trim(filled_index_grid, img.pieces)
+	filled_index_grid = forest.fill(best_grid, best_extra_list, img)
+	pixels = best_forest.reconstruct_trim(filled_index_grid, img.pieces)
 	cv2.imwrite(reconDir + 'filled_index_grid.jpg', pixels)
 
 	correct = direct_metric(img, filled_index_grid)
-	print 'direct correct', correct, '/', frame_W * frame_H
-	correct2 = neighbor_metric(img, filled_index_grid)
-	print 'neighbor correct', correct2, '/', 2*frame_W*frame_H - frame_H - frame_W
-	with open('./out.txt', 'w') as f:
-  		f.write('%d%s%d' % (correct, '/', frame_W * frame_H))
-  		f.write('\n')
-  		f.write('%d%s%d' % (correct2, '/', 2*frame_W*frame_H - frame_H - frame_W))
+	answer = []
+	for i in xrange(K):
+		answer.append(str(filled_index_grid[frame_H - 1 - i // frame_W, i % frame_W, 0]))
+		
+	with open('./' + picName + '_out.txt', 'w') as f:
+  		f.write(' '.join(answer))
 
 def mixed_test():
 	type2 = True
@@ -178,7 +242,7 @@ def mixed_test():
 		sq.compute_mean_and_covar_inv()
 
 	img2 = Image(img_filename2)
-	scramble(img2, type2)
+	# scramble(img2, type2)
 	K2 = len(img2.pieces)
 	for sq in img2.pieces:
 		sq.compute_mean_and_covar_inv()
@@ -306,7 +370,14 @@ def save_ssd_wrong_pics():
 				'./Images/differences/diff' + str(count) + '.jpg')
 			count += 1
 
+
+#img_filename = './Images/elephants/elephant.jpg'
+for i in xrange(2752, 2753):
+	fileName = './images/huawei_' + str(P) + '/' + str(i) + '.png'
+#	fileName = '../../Pictures/data_test1_blank/' + str(P) + '/' + str(i).zfill(4) + '.png'
+	dsf_reconstruction_test(fileName)
+
 # compute_edge_correct_percents()
-dsf_reconstruction_test()
+# dsf_reconstruction_test()
 # dsf_reconstruct_dataset()
 # mixed_test()
