179f1317e8b8491bce5d374f9a04a4a615421fec
didgogns <34927608+didgogns@users.noreply.github.com>
2020-02-09 23:00:20 +0900

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,21 +1,22 @@
-img_filename = './Images/elephants/elephant.jpg'
-# img_filename = './Images/imData/8.png'
-img_filename2 = './Images/imData/13.png'
-
-# ./Images/imData/ for the MIT dataset and ./Images/elephants/ for the elephant picture
-img_folder = './Images/imData/'
+#img_filename = './Images/elephants/elephant.jpg'
 
 # the side length (in pixels) of the nxn square puzzle piece
 # 64 for the elephant and 28 for the MIT data
-P = 28
+P = 16
+# P = 32
+# P = 64
 
 # epsilon factor to add to distances so that no dividing by zero
 eps = 0.000001
 
 # the number of buckets to put edges into (per color channel)
-num_buckets = 32
+# num_buckets = 1
+num_buckets = 1
+# num_buckets = 1
 
 # the minimum number of edge weights that will be checked per square.
 # Should be at least 5 since it can use four for the edges of that same square
 # not super safe for type1, could potentially throw out everything if they all have the wrong rotation
-min_edges = 250
+# min_edges = 256
+# min_edges = 512
+min_edges = 50
